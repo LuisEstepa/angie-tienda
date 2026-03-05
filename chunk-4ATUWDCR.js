4e7469d435b5f3c6e235d3c8c7fd80e8c335e70b
Luis Estepa <116852447+LuisEstepa@users.noreply.github.com>
2026-03-05 18:22:31 -0500

    Auto-generated commit

---
+++ chunk-4ATUWDCR.js
@@ -0,0 +1,1 @@
+import{Ia as o,Ya as c,Za as n,jb as a}from"./chunk-PFQ4G2YP.js";var s=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-checkout"]],decls:2,vars:0,template:function(e,p){e&1&&(c(0,"p"),a(1,"checkout works!"),n())},encapsulation:2})};export{s as Checkout};
